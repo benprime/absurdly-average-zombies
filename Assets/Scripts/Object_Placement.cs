4b0dba61d46791a23d7c2b44ee71f249a8d4bc28
Nicholas Mack <nichmack@gmail.com>
2015-05-27 23:20:16 -0700

    Added turret placement on mouse click and temporary practice grid placement

---
+++ Assets/Scripts/Object_Placement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Object_Placement : MonoBehaviour {
+	public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonUp (0)) {
+			Vector3 target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			target.z = 0;
+			Instantiate (obj, target, Quaternion.identity);
+		}
+	}
+
+	public void SelectTurretType(GameObject turret) {
+		obj = turret;
+	}
+}
