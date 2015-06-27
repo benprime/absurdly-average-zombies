f1be1816fb104557d68df0c33bf07255e9fe5886
Nicholas Mack <nichmack@gmail.com>
2015-06-26 17:55:13 -0700

    WIP: Implemented start of zombie health bars

---
+++ Assets/Scripts/UI/UI_FloatingHealthBar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_FloatingHealthBar : MonoBehaviour {
+	public Image healthBarPrefab;
+	public float barOffset = 10f;
+	public Transform uiCanvas;
+	private Image healthBar;
+
+	// Use this for initialization
+	void Start () {
+		healthBar = Instantiate(healthBarPrefab) as Image;
+		healthBar.transform.SetParent (uiCanvas, false);
+		healthBar.transform.position.Set(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthBar.rectTransform.position = transform.position;
+		//healthBar.rectTransform.position.y += barOffset;  //TODO: Make this work
+		//healthBar.fillAmount = targetHealth/100;  //TODO: Hook bar to target health and modify with damage/heal
+	}
+}
