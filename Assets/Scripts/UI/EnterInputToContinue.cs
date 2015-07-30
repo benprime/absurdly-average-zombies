4acd1db5321fca304d15db5ea9f8ef1618b1e90c
Nicholas Mack <nichmack@gmail.com>
2015-07-30 01:54:18 -0700

    Overhaul of menus and added company logo

---
+++ Assets/Scripts/UI/EnterInputToContinue.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterInputToContinue : MonoBehaviour {
+	public string nextLevel = "SelectLevel";
+	public bool changesAutomatically = false;
+	public float autoTime = 5f;
+	private float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Input.touchCount);
+		if(Input.anyKey || (Input.touchCount > 0)) {
+			Application.LoadLevel (nextLevel);
+		}
+		if(changesAutomatically) {
+			timer += Time.deltaTime;
+			if(timer >= autoTime) {
+				Application.LoadLevel (nextLevel);
+			}
+		}
+	}
+}
