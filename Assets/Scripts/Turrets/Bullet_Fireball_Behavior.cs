6a9e9ca14eb8da6e5a52a7ddc55c70725084ea61
Ben Smith <bsmithpct@gmail.com>
2015-07-30 00:43:40 -0700

    Added prefab for flamethrower.

---
+++ Assets/Scripts/Turrets/Bullet_Fireball_Behavior.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet_Fireball_Behavior : MonoBehaviour {
+	public float moveSpeed = 20f;
+	public Vector3 startPos;
+	public float range = 10f;
+	public float damage = 5f;
+
+	// Use this for initialization
+	protected virtual void Awake () {
+		startPos = new Vector3();
+		startPos = transform.position;
+
+		float z = (Mathf.PingPong (Time.time * 100, 30) - 15);
+		transform.Rotate (0.0f, 0.0f, z);
+		//transform.up += new Vector3 (0, 0, z);
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+		transform.Translate (Vector2.up * moveSpeed * Time.deltaTime);
+		if(Vector3.Distance (transform.position, startPos) > range) {
+			Destroy (this.gameObject);
+		}
+	}
+
+	protected virtual void OnTriggerEnter2D (Collider2D other) {
+		if(other.tag == "enemy") {
+			other.SendMessage("TakeDamage", this.damage);
+			Destroy (this.gameObject);
+		}
+	}
+
+}
