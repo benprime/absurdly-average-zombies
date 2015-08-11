9d4f072c769af2fce972bcfe41fb8e92993f6b9d
Nicholas Mack <nichmack@gmail.com>
2015-08-11 01:22:41 -0700

    Added lock icons to level select screen

---
+++ Assets/Scripts/UI/LockLevelButtons.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LockLevelButtons : MonoBehaviour {
+	public string goToLevelName;
+	public Image lockIcon;
+
+	// Use this for initialization
+	void Start () {
+		if(GameManager.instance.progressManager.IsLevelLocked(goToLevelName)) {
+			GetComponent<Button>().enabled = false;
+			gameObject.GetComponentInChildren<Text>().enabled = false;
+			lockIcon.enabled = true;
+		}
+		else {
+			GetComponent<Button>().enabled = true;
+			gameObject.GetComponentInChildren<Text>().enabled = true;
+			lockIcon.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
