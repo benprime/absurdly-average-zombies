65e877c804e171f9cfff46e4cb709acef8c1bafe
Ben Smith <bsmitpct@gmail.com>
2016-02-23 21:29:23 -0800

    Map position adjustments.

--- Assets/Scripts/_GameManagement/MapLoader.cs
+++ Assets/Scripts/_GameManagement/MapLoader.cs
@@ -21,8 +21,13 @@ public class MapLoader : MonoBehaviour
     {
 
 		GameObject map = GameObject.Find("Map");
-		GameObject mapTiles = new GameObject();
-        mapTiles.transform.SetParent(map.transform);
+
+        // this is how we handle pixelPerUnit
+        Vector3 offScale = new Vector3(.96f, .96f, 1f);
+        map.transform.localScale = offScale;
+
+        //GameObject mapTiles = new GameObject();
+        //mapTiles.transform.SetParent(map.transform);
 
         // TODO: Maybe someday make this loader take other map formats into account.
         // for now, we'll make some basic assumptions (single layer 2d orthogonal)
@@ -91,7 +96,7 @@ public class MapLoader : MonoBehaviour
 
                 // grab the prefab tile based on gid
                 GameObject tile = new GameObject("Tile");// tiles[gid - 1];
-				tile.transform.SetParent(mapTiles.transform);
+				tile.transform.SetParent(map.transform);
                 tile.transform.localPosition = new Vector3(x - (widthInTiles / 2.0f) + 0.5f, y - (heightInTiles / 2.0f) - 0.5f);
 
                 tile.layer = 8; // terrain layer
@@ -133,9 +138,6 @@ public class MapLoader : MonoBehaviour
             }
 		}
 
-        // this is how we handle pixelPerUnit
-		Vector3 offScale = new Vector3 (.96f, .96f, 1f);
-		mapTiles.transform.localScale = offScale;
     }
 
     // Update is called once per frame
