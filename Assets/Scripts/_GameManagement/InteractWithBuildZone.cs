2fd3b3bbb21225f3fb657443f908fe53faed0cc4
Ben Smith <bsmitpct@gmail.com>
2016-02-25 20:47:59 -0800

    When a radial menu is open, and another build zone is clicked, the clicked build zone now pops the menu.

--- Assets/Scripts/_GameManagement/InteractWithBuildZone.cs
+++ Assets/Scripts/_GameManagement/InteractWithBuildZone.cs
@@ -19,37 +19,28 @@ public class InteractWithBuildZone : MonoBehaviour
 
     void Update()
     {
-        //int pointerId = (Input.touchCount == 1) ? Input.GetTouch(0).fingerId : -1;  //Only accounts for single touches TODO: make it work nicely for accidental multi touches
-
         RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Camera.main.transform.forward, 20, LayerMask.GetMask("BuildZone", "UI"));
 
         // over a game object?
-        bool isOverGameObject = false;
-
-        if (EventSystem.current.IsPointerOverGameObject())
-            isOverGameObject = true;
+        bool isOverUIButton = EventSystem.current.IsPointerOverGameObject();
 
         foreach (Touch t in Input.touches)
         {
             if (EventSystem.current.IsPointerOverGameObject(t.fingerId))
-                isOverGameObject = true;
+            {
+                isOverUIButton = true;
+                break;
+            }
         }
+        bool isSingleTouchEnding = (Input.touchCount == 1) ? (Input.GetTouch(0).phase == TouchPhase.Ended && Input.GetTouch(0).phase == TouchPhase.Canceled) : false; //check to see if there is touch input, and if so, if the touch just ended
 
-        if (hitInfo && !isOverGameObject)
-        { //mouse is over a BuildZone && do not place object when mouse is over button
+        // mouse is over a BuildZone && do not place object when mouse is over button
+        if (hitInfo && !isOverUIButton)
+        {
             GameObject hitZone = hitInfo.transform.gameObject;
 
-            /*
-            if (Input.GetMouseButtonDown(0))
-            {
-                //hitSprite.color = Color.green;  //TODO: make flag in BuildZone class that sets color when mouse is over it
-            }
-            */
-
-            bool isSingleTouchEnding = (Input.touchCount == 1) ? (Input.GetTouch(0).phase == TouchPhase.Ended && Input.GetTouch(0).phase == TouchPhase.Canceled) : false; //check to see if there is touch input, and if so, if the touch just ended
             if (Input.GetMouseButtonUp(0) || isSingleTouchEnding)
             {
-                if (lastHit && lastHit.GetComponent<BuildZone>().menuOpen) return;  //temp fix for bug #1
                 if (lastHit && lastHit != hitZone) lastHit.GetComponent<BuildZone>().CloseOut();
                 lastHit = hitZone;
 
@@ -57,10 +48,11 @@ public class InteractWithBuildZone : MonoBehaviour
             }
         }
         else
-        { //mouse is not over a BuildZone			
-            if (!isOverGameObject)
+        {
+            // mouse is not over a BuildZone			
+            if (!isOverUIButton)
             {
-                if (Input.GetMouseButtonDown(0))
+                if (Input.GetMouseButtonDown(0) || isSingleTouchEnding)
                 {
                     if (lastHit) lastHit.GetComponent<BuildZone>().CloseOut();
                 }
