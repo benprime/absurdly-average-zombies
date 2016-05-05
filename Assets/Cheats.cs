498e33efd560d2b3de888cc986d9c55a53ce071c
Ben Smith <bsmithpct@gmail.com>
2016-05-04 23:14:38 -0700

    Updated menu button images.  Added cheat option for level select screen.

---
+++ Assets/Cheats.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cheats : MonoBehaviour {
+
+    public void UnlockAllLevels()
+    {
+        foreach(LevelProgressData data in GameManager.instance.progressManager.LevelProgress)
+        {
+            data.Locked = false;
+        }
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
