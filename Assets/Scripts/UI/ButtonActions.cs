ea4b511bbeb7e9a4100fa492402b55f764156fce
Nicholas Mack <nichmack@gmail.com>
2016-05-25 22:43:47 -0500

    Confirmation button for killing saves. Fix bug #68

--- Assets/Scripts/UI/ButtonActions.cs
+++ Assets/Scripts/UI/ButtonActions.cs
@@ -20,9 +20,11 @@ public class ButtonActions : MonoBehaviour
 		GameManager.instance.ToggleAudio ();
 	}
 
-    public void DeleteSaveFile()
+	public void DeleteSaveFile(GameObject window)
     {
-        GameManager.instance.progressManager.ClearData();
+		GameManager.instance.progressManager.ClearData();
+		if (window) Destroy(window);
+		else Destroy(gameObject);
     }
 
     public void ShowCredits(GameObject creditReel)
