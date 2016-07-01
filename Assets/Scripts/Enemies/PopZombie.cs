b3666d8e3c2683a7992a99ee2c34a1e3b0657953
Nicholas Mack <nichmack@gmail.com>
2016-06-30 20:13:33 -0500

    Pop zombies now puke out a stream of new zombies

--- Assets/Scripts/Enemies/PopZombie.cs
+++ Assets/Scripts/Enemies/PopZombie.cs
@@ -8,7 +8,8 @@ public class PopZombie : Zombie
 {
 	public GameObject smZombiePrefab, mdZombiePrefab, lgZombiePrefab;
 	public int minToPop = 3, maxToPop = 5;
-	private int numToPop = 3;
+	private int numToPop = -1;
+	public float zombieSpaceDelay = .5f;
 
     // Use this for initialization
     protected override void Start()
@@ -24,34 +25,40 @@ public class PopZombie : Zombie
 
 	protected override void Die ()
 	{
-		numToPop = Random.Range (minToPop, maxToPop + 1);
-		for(int i = 0; i < numToPop; i++) 
-			PopZombies ();
-		base.Die ();
+		if (numToPop < 0) {
+			numToPop = Random.Range (minToPop, maxToPop + 1);
+			GetComponent<SpriteRenderer> ().enabled = false;
+			StartCoroutine (PopZombies (numToPop));
+		}
 	}
 
-	public void PopZombies()
+	public IEnumerator PopZombies(int numToPop)
 	{
-		ZombieSize size;
-		float rando = Random.Range(0f, 1f);
-		if (rando < .6)
-			size = ZombieSize.Medium;
-		else if (rando < .99)
-			size = ZombieSize.Small;
-		else
-			size = ZombieSize.Large;
+		for (int i = 0; i < numToPop; i++) { 
+			ZombieSize size;
+			float rando = Random.Range (0f, 1f);
+			if (rando < .6)
+				size = ZombieSize.Medium;
+			else if (rando < .99)
+				size = ZombieSize.Small;
+			else
+				size = ZombieSize.Large;
 
-		GameObject z = null;
+			GameObject z = null;
 
-		if (size == ZombieSize.Small && smZombiePrefab) 
-			z = Instantiate(smZombiePrefab, this.transform.position, Quaternion.identity) as GameObject;
-		else if (size == ZombieSize.Medium && mdZombiePrefab) 
-			z = Instantiate(mdZombiePrefab, this.transform.position, Quaternion.identity) as GameObject;
-		else if (size == ZombieSize.Large && lgZombiePrefab) 
-			z = Instantiate(lgZombiePrefab, this.transform.position, Quaternion.identity) as GameObject;
+			if (size == ZombieSize.Small && smZombiePrefab)
+				z = Instantiate (smZombiePrefab, this.transform.position, Quaternion.identity) as GameObject;
+			else if (size == ZombieSize.Medium && mdZombiePrefab)
+				z = Instantiate (mdZombiePrefab, this.transform.position, Quaternion.identity) as GameObject;
+			else if (size == ZombieSize.Large && lgZombiePrefab)
+				z = Instantiate (lgZombiePrefab, this.transform.position, Quaternion.identity) as GameObject;
 
-		if (z) {
-			z.GetComponent<Zombie> ().SetPath (path, currentNodeIndex);
+			if (z) {
+				z.GetComponent<Zombie> ().SetPath (path, currentNodeIndex);
+			}
+
+			yield return new WaitForSeconds (zombieSpaceDelay);
 		}
+		base.Die ();
 	}
 }
