eaaa1be39b50d20fd362ec7e29f0b97751b9f94b
Ben Smith <bsmithpct@gmail.com>
2016-09-01 23:44:53 -0700

    Cleaned up zombie base class, reduced calls in an effort to improve performance.

--- Assets/Scripts/Enemies/Zombie.cs
+++ Assets/Scripts/Enemies/Zombie.cs
@@ -13,7 +13,7 @@ public enum ZombieSize
     Small,
     Medium,
     Large,
-	Pop,
+    Pop,
     Boss
 }
 
@@ -23,7 +23,6 @@ public class Zombie : MonoBehaviour
     public float moveSpeed = 2f;
     public float moveModifier = 1.0f;
     public float turnSpeed = 4f;
-    public float walkSwayModifier = 20;
     public float maxHitPoints = 10f;
     public float hitPoints = 10f;
     public int worthCurrency = 5;
@@ -38,8 +37,8 @@ public class Zombie : MonoBehaviour
     public float flameDamageInterval = 1f;
     public float fireDamage = 0;
     protected float nextFlameDamageTime = 0f;
-	protected int fireTimer = 5;
-	public int fireDuration = 5;
+    protected int fireTimer = 5;
+    public int fireDuration = 5;
 
     public ParticleSystem hitParticleSystem;
     public ParticleSystem deathParticleSystem;
@@ -49,21 +48,34 @@ public class Zombie : MonoBehaviour
     protected ParticleSystem psSmallLeft;
     protected ParticleSystem psSmallRight;
 
-
     public bool onFire = false;
-
     public ZombieState zombieState;
-
-    public Vector3 direction;
-    protected int randSwayStart;
-
+    private Vector3 direction;
     protected GameManager gm;
 
     public ZombiePath path = null;
-    protected int currentNodeIndex = 0;
     public float targetCloseness = .5f;
 
     public GameObject popNums;
+
+    private int _currentNodeIndex;
+    protected int currentNodeIndex
+    {
+        get
+        {
+            return _currentNodeIndex;
+        }
+        set
+        {
+            _currentNodeIndex = value;
+            direction = currentNode.transform.position - transform.position;
+        }
+    }
+
+    public GameObject currentNode
+    {
+        get { return path.nodes[currentNodeIndex]; }
+    }
 
     void Awake()
     {
@@ -77,26 +89,17 @@ public class Zombie : MonoBehaviour
 
         this.zombieState = ZombieState.Normal;
         // set travel direction
-        //this.direction = new Vector3(Random.Range (-1f, 1f), Random.Range (-1f, 1f), 0);
-        this.direction = Vector3.down;
-
-        // without this new zombies kind of just stand around
-        this.direction = this.direction.normalized * 1;
-
-        // set initial rotation of zombie
-        this.transform.up = this.direction;
-
-        // randomize initial sway
-        // so zombies don't all sway at the same time
-        this.randSwayStart = Random.Range(0, 10) * 100;
-        this.walkSwayModifier = Random.Range(20, 35);
+        if (currentNode)
+        {
+            this.direction = currentNode.transform.position - this.transform.position;
+        }
 
         // save reference to particle systems for later use
         this.psLarge = this.transform.FindChild("ParticleSystemFireLarge").GetComponent<ParticleSystem>();
         this.psSmallLeft = this.transform.FindChild("ParticleSystemFireSmallLeft").GetComponent<ParticleSystem>();
         this.psSmallRight = this.transform.FindChild("ParticleSystemFireSmallRight").GetComponent<ParticleSystem>();
 
-		hitPoints = maxHitPoints;
+        hitPoints = maxHitPoints;
     }
 
     // Update is called once per frame
@@ -107,6 +110,47 @@ public class Zombie : MonoBehaviour
             return;
         }
 
+        CheckAndUpdateFireDamage();
+
+        if (this.hitPoints <= 0)
+        {
+            this.Die();
+            return;
+        }
+
+        PathMovement();
+    }
+
+    private void PathMovement()
+    {
+        // check if there are paths to process
+        if (path.nodes.Count <= 0)
+            return;
+
+        // update physical location of zombie
+        transform.position = Vector2.MoveTowards(transform.position, currentNode.transform.position,
+            moveSpeed * Time.deltaTime * moveModifier);
+
+        // rotate zombie toward direction of travel
+        if (transform.up != direction)
+        {
+            var newDir = Vector3.RotateTowards(transform.up, direction, turnSpeed * Time.deltaTime * moveModifier, 0.0f);
+            transform.up = newDir;
+        }
+
+        // check for collision with path collider
+        if (Vector2.Distance(transform.position, currentNode.transform.position) < targetCloseness)
+        {
+            // if we're not on the last path collider, move on
+            if (currentNodeIndex < path.nodes.Count - 1)
+            {
+                currentNodeIndex++;
+            }
+        }
+    }
+
+    private void CheckAndUpdateFireDamage()
+    {
         if (this.onFire)
         {
             if (Time.time > nextFlameDamageTime)
@@ -114,8 +158,8 @@ public class Zombie : MonoBehaviour
                 nextFlameDamageTime += this.flameDamageInterval;
                 this.TakeDamage(this.fireDamage, DamageType.light);
                 //this.fireDamage--;
-				this.fireTimer--;
-				if (this.fireTimer <= 0)
+                this.fireTimer--;
+                if (this.fireTimer <= 0)
                 {
                     psLarge.Stop();
                     psSmallLeft.Stop();
@@ -125,41 +169,6 @@ public class Zombie : MonoBehaviour
                 }
             }
         }
-
-        if (this.hitPoints <= 0)
-        {
-            this.Die();
-            return;
-        }
-
-        // update zombie position, moving the direction
-        GameObject currentNode = path.nodes[currentNodeIndex];
-        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
-        direction = currentNode.transform.position - transform.position;
-        var newDir = Vector3.RotateTowards(transform.up, direction, turnSpeed * Time.deltaTime * moveModifier, 0.0f);
-
-        if (transform.up == direction)
-            Sway();
-        else
-            transform.up = newDir;
-
-        if (path.nodes.Count <= 0)
-            return;
-
-        transform.position = Vector2.MoveTowards(currentPosition, currentNode.transform.position, moveSpeed * Time.deltaTime * moveModifier);
-
-        if (Vector2.Distance(currentPosition, currentNode.transform.position) < targetCloseness && currentNodeIndex < path.nodes.Count - 1)
-            currentNodeIndex++;
-    }
-
-    protected void Sway()
-    {
-        // z is -10 to 10 sway (in degrees)
-        float z = Mathf.PingPong(Time.time * this.walkSwayModifier + randSwayStart, 20) - 10;
-
-        // apply the "sway" rotate
-        //transform.position.z = z;
-        transform.Rotate(0.0f, 0.0f, z);
     }
 
     public enum DamageType { light, medium, heavy }
@@ -172,10 +181,6 @@ public class Zombie : MonoBehaviour
             this.hb.healthBar.rectTransform.localScale = new Vector3(Mathf.Max(0, (hitPoints / maxHitPoints)), 1, 1); //TODO: Make healthbar scale from left pivot point
         }
 
-        //if(hitPoints <= 0) Destroy (hb.healthBar.gameObject);
-        //else {
-        //this.GeneratePopUpNumber(amount.ToString(), Color.red, false);
-
         if (hitPoints <= maxHitPoints / 3)
         {
             hb.healthBar.color = Color.red;
@@ -184,22 +189,17 @@ public class Zombie : MonoBehaviour
         {
             hb.healthBar.color = Color.yellow;
         }
-        //}
 
         GameObject.Instantiate(this.hitParticleSystem, this.transform.position, Quaternion.identity);
-        //localBloodsObj.transform.rotation = rotation;
-		//localBloodsObj.transform.localRotation; 
-        //localBloodsObj.Play();
     }
 
     protected virtual void Die()
-	{
+    {
         this.zombieState = ZombieState.Dead;
         CircleCollider2D c = GetComponent<CircleCollider2D>();
         c.enabled = false;
 
-        // TODO: set animation to death animation (via trigger)
-		GameManager.instance.PlayerCurrencyTransaction (worthCurrency);
+        GameManager.instance.PlayerCurrencyTransaction(worthCurrency);
         this.GeneratePopUpNumber("$" + worthCurrency, Color.yellow, true);
 
         Destroy(this.hb.healthBar.gameObject);
@@ -209,10 +209,6 @@ public class Zombie : MonoBehaviour
         GameObject.Instantiate(this.deathParticleSystem, this.transform.position, Quaternion.identity);
     }
 
-    //	void OnDestroy() {
-    //		if(this.zombieState != ZombieState.Dead && this.hb.healthBar.gameObject != null) Destroy(this.hb.healthBar.gameObject);
-    //	}
-    //
     protected void OnCollisionStay2D(Collision2D other)
     {
         //damage the buildings/turrets in path
@@ -228,18 +224,18 @@ public class Zombie : MonoBehaviour
         this.CatchFire(4);
     }
 
-	public void CatchFire(float dmg)
-	{
-		// fire dps duration timer will be reset everytime zombie is hit by a fireball
-		this.fireTimer = fireDuration;
+    public void CatchFire(float dmg)
+    {
+        // fire dps duration timer will be reset everytime zombie is hit by a fireball
+        this.fireTimer = fireDuration;
 
         // getting hit by another fireball will only reset damage if the fireball stats are higher than what is already on the zombie
-		if(dmg > this.fireDamage) this.fireDamage = dmg;
+        if (dmg > this.fireDamage) this.fireDamage = dmg;
 
         // we only set fire to zombie if not already on fire
         if (!this.onFire)
         {
-			this.nextFlameDamageTime = Time.time + this.flameDamageInterval;
+            this.nextFlameDamageTime = Time.time + this.flameDamageInterval;
 
             // start the flame particle effect
             psLarge.Play();
@@ -285,8 +281,10 @@ public class Zombie : MonoBehaviour
         return 1f;
     }
 
-	public void SetPath(ZombiePath path, int startNodeIndex) {
-		this.path = path;
-		this.currentNodeIndex = startNodeIndex;
-	}
+
+    public void SetPath(ZombiePath path, int startNodeIndex)
+    {
+        this.path = path;
+        this.currentNodeIndex = startNodeIndex;
+    }
 }
