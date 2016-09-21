067bd4841299434f9c3b1a5f867fdfab9f3daaba
Ben Smith <bsmithpct@gmail.com>
2016-09-20 21:57:35 -0700

    Fix for pickup placement.

--- Assets/Scripts/PickUps/PickUp_SpawnZone.cs
+++ Assets/Scripts/PickUps/PickUp_SpawnZone.cs
@@ -13,6 +13,8 @@ public class PickUp_SpawnZone : MonoBehaviour
 
     private IWaveGenerator wg;
 
+    private BuildZone[] buildZones;
+
     // Use this for initialization
     void Start()
     {
@@ -23,6 +25,7 @@ public class PickUp_SpawnZone : MonoBehaviour
         {
             wg = FindObjectOfType<WaveGeneratorTutorial>();
         }
+        buildZones = FindObjectsOfType<BuildZone>();
     }
 
     // Update is called once per frame
@@ -35,10 +38,29 @@ public class PickUp_SpawnZone : MonoBehaviour
                 spawnRate = Random.Range(2.0f, 5.0f);
                 nextSpawn = Time.time + spawnRate;
                 Vector2 spawnBox = new Vector2(transform.localScale.x, transform.localScale.y);
-                Vector2 randomPos = new Vector2(Random.Range(-.5f, .5f) * spawnBox.x, Random.Range(-.5f, .5f) * spawnBox.y);
+                Vector2 randomPos;
+                do
+                {
+                    randomPos = new Vector2(Random.Range(-.5f, .5f) * spawnBox.x, Random.Range(-.5f, .5f) * spawnBox.y);
+
+                } while (!isPositionValid(randomPos));
+
                 Instantiate(pickUpPrefab, randomPos, Quaternion.identity);
             }
         }
         else nextSpawn = Time.time + spawnRate;
+    }
+
+    private bool isPositionValid(Vector3 pos)
+    {
+        foreach (BuildZone bz in buildZones)
+        {
+            BoxCollider2D boxCollider = bz.gameObject.GetComponent<BoxCollider2D>();
+            if (boxCollider.bounds.Contains(pos))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
